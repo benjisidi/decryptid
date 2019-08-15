5c2cde6bb9a87d782661d94db4c3022960b622bd
Benjamin Sidi <benjisidi@gmail.com>
2019-08-15 19:26:30 +0100

    Piece input and rotation working

--- Board.py
+++ Board.py
@@ -1,5 +1,6 @@
 from tkinter import Tk, Canvas
 from collections import defaultdict
+from copy import deepcopy
 import math
 
 # ToDo: Clean up namedtuples being duplicated
@@ -8,13 +9,11 @@ class Board:
     cos60 = math.cos(math.radians(60))
     sin60 = math.sin(math.radians(60))
 
-    def __init__(self, dims, origin = {'x': 100, 'y': 100}, terrain={}, territories={}, structures={}):
+    def __init__(self, dims, pieces, pieceOrder, structures = {}, origin = {'x': 100, 'y': 100}):
         self.dims = dims
         self.hexWidth = self.dims['length']*(1+Board.cos60)
         self.hexHeight = self.dims['length'] * Board.sin60
         self.origin = origin
-        self.terrain = terrain
-        self.territories = territories
         self.structures = structures
         self.terrainColors = {
             'desert': '#ebc034',
@@ -27,24 +26,58 @@ class Board:
             'bear': '#000',
             'cougar': '#F00'
         }
+        self.terrains = {
+            'desert': [],
+            'forest': [],
+            'mountain': [],
+            'swamp': [],
+            'water': []
+        }
+        self.territories = {
+            'bear': [],
+            'cougar': []
+        }
         self.tiles = {}
-        # Terrain contains one entry for each tile, so iterate over that and make an entry for each hex, then 
-        # update the rest of their attributes
-        for environment in terrain:
-            for tile in terrain[environment]:
-                self.tiles[self.tileKey(*tile)] = {'terrain': environment}
-                self.tiles[self.tileKey(*tile)]['coords'] = tile
-        for territory in territories:
-            for tile in territories[territory]:
-                self.tiles[self.tileKey(*tile)]['territory'] = territory
+        self.loadPieces(pieces, pieceOrder)
         for tileKey in structures:
             self.tiles[tileKey]['structure'] = structures[tileKey]
+
+    def rotatePiece(self, piece):
+        flippedPiece = deepcopy(piece)
+        flip = lambda coord: [int(math.fabs(i - 5)) for i in coord]
+        for environment in flippedPiece['terrain']:
+            flippedPiece['terrain'][environment] = [flip(tile) for tile in flippedPiece['terrain'][environment]]
+        for animal in flippedPiece['territories']:
+            flippedPiece['territories'][animal] = [flip(tile) for tile in flippedPiece['territories'][animal]]
+        return flippedPiece
+
+    def loadPieces(self, pieces, pieceOrder):
+        vertMultiplier = [0, 1, 0, 1, 0, 1]
+        horizMultiplier = [0, 0, 1, 1, 2, 2]
+        for (i, pieceNumber) in enumerate(pieceOrder):
+            index = int(math.fabs(pieceNumber))
+            piece = pieces[index]
+            if pieceNumber < 0:
+                piece = self.rotatePiece(piece)
+            for environment in piece['terrain']:
+                for tile in piece['terrain'][environment]:
+                    tile = [tile[0] + vertMultiplier[i]*6, tile[1] + horizMultiplier[i]*6]
+                    tileKey = self.getTileKey(*tile)
+                    self.tiles[tileKey] = {'terrain': environment}
+                    self.tiles[tileKey]['coords'] = tile
+                    self.terrains[environment].append(tileKey)
+            for territory in piece['territories']:
+                for tile in piece['territories'][territory]:
+                    tile = [tile[0] + vertMultiplier[i]*6, tile[1] + horizMultiplier[i]*6]
+                    tileKey = self.getTileKey(*tile)
+                    self.tiles[tileKey]['territory'] = territory
+                    self.territories[territory].append(tileKey)
     
-    def tileKey(self, col, row):
+    def getTileKey(self, col, row):
         return f'{col}-{row}'
 
     def getTile(self, col, row):
-        return self.tiles[self.tileKey(col, row)]
+        return self.tiles[self.getTileKey(col, row)]
 
     def indexToCoords(self, col, row):
         return [self.origin['x'] + (col * self.hexWidth), self.origin['y'] + (row * self.hexHeight)]
@@ -67,22 +100,34 @@ class Board:
         y +=  self.hexHeight - r
         callbacks[tile['structure'][0]](x, y, r, tile['structure'][1], canvas)
 
-    def drawHex(self, x, y, col, row, canvas):
+    def drawHex(self, x, y, col, row, canvas, fillColor='', outlineColor='#FFF', width=2, length=False):
+        if not length:
+            length = self.dims['length']
         points = [
             x, y,
-            x+self.dims['length'], y,
-            x+self.dims['length']+self.dims['length']*Board.cos60, y+self.dims['length']*Board.sin60,
-            x+self.dims['length'], y+2*self.dims['length']*Board.sin60,
-            x, y+2*self.dims['length']*Board.sin60,
-            x-self.dims['length']*Board.cos60, y+self.dims['length']*Board.sin60
+            x+length, y,
+            x+length+length*Board.cos60, y+length*Board.sin60,
+            x+length, y+2*length*Board.sin60,
+            x, y+2*length*Board.sin60,
+            x-length*Board.cos60, y+length*Board.sin60
         ]
-        tile = self.getTile(col, row)
-        outlineColor = '#FFF' if 'territory' not in tile else self.territoryColors[tile['territory']] 
-        fillColor = self.terrainColors[tile['terrain']]
         canvas.create_polygon(points, outline=outlineColor, fill=fillColor, width=2)
+        canvas.pack(fill="both", expand=True)
+
+
+    def drawTile(self, x, y, col, row, canvas, length=False):
+        if not length:
+            length = self.dims['length']
+        tile = self.getTile(col, row)
+        # outlineColor = '#FFF' if 'territory' not in tile else self.territoryColors[tile['territory']] 
+        fillColor = self.terrainColors[tile['terrain']]
+        self.drawHex(x, y, col, row, canvas, fillColor=fillColor)
         if ('structure' in tile):
             self.drawStructure(tile, canvas)
-        canvas.pack(fill="both", expand=True)
+        if ('territory' in tile):
+            territoryColor = self.territoryColors[tile['territory']]
+            # ToDo: Eliminate magic numbers
+            self.drawHex(x + 4, y + 7, col, row, canvas, outlineColor=territoryColor, width=2, length=self.dims['length']- 8)
 
     def drawBoard(self, width=1280, height=720):
         root = Tk()
@@ -92,7 +137,7 @@ class Board:
         def drawCols(startIndex):
             for col in range(startIndex, self.dims['cols'], 2):
                 for row in range(startIndex, self.dims['rows'], 2):
-                    self.drawHex(*self.indexToCoords(col, row), col, row, c)
+                    self.drawTile(*self.indexToCoords(col, row), col, row, c)
         # Draw the even columns first
         drawCols(0)
         # Now draw the odd columns
--- decryptid.py
+++ decryptid.py
@@ -1,11 +1,76 @@
+from itertools import product
 from Board import Board
 from collections import namedtuple
 
 pieces = [
     {}, # There is no piece 0
-    {},
-    {},
-    {},
+    # Piece 1
+    {
+        'terrain': {
+            'desert': [
+                [2, 4], [3, 3], [3, 5], [4, 4]
+            ],
+            'forest': [
+                [4, 0], [4, 2], [5, 1], [5, 3], [5, 5]
+            ],
+            'swamp': [
+                [0, 2], [0, 4], [1, 3], [1, 5]
+            ],
+            'water': [
+                [0, 0], [1, 1], [2, 0], [2, 2], [3, 1]
+            ]
+        },
+        'territories': {
+            'bear': [
+                [3, 5], [4, 4]
+            ]
+        }
+    },
+    # Piece 2
+    {
+        'terrain': {
+            'desert': [
+                [3, 3], [4, 2], [5, 3], [5, 5]
+            ],
+            'forest': [
+                [1, 1], [2, 0], [2, 2], [3, 1], [4, 0], [5, 1]
+            ],
+            'mountain': [
+                [1, 5], [2, 4], [3, 5], [4, 4]
+            ],
+            'swamp': [
+                [0, 0], [0, 2], [0, 4], [1, 3]
+            ]
+        },
+        'territories': {
+            'cougar': [
+                [0, 0], [1, 1], [2, 0]
+            ]
+        }
+    },
+    # Piece 3
+    {
+        'terrain': {
+            'forest': [
+                [2, 0], [2, 2], [3, 1], [4, 0]
+            ],
+            'mountain': [
+                [0, 4], [1, 5], [2, 4], [3, 3], [3, 5]
+            ],
+            'swamp': [
+                [0, 0], [0, 2], [1, 1], [1, 3]
+            ],
+            'water': [
+                [4, 2], [4, 4], [5, 1], [5, 3], [5, 5]
+            ]
+        },
+        'territories': {
+            'cougar': [
+                [0, 2], [0, 4], [1, 3]
+            ]
+        }
+    },
+    # Piece 4
     {
         'terrain': {
             'desert': [
@@ -14,41 +79,97 @@ pieces = [
                 [2, 4]
             ],
             'mountain': [
-                [2, 0], [2, 2],
-                [3, 1], 
-                [4, 0],
-                [5, 1]
-            ],
-            'water': [
-                [3, 3],
-                [4, 2],
-                [5, 3]
+                [2, 0], [2, 2], [3, 1], 
+                [4, 0], [5, 1]
             ],
             'forest': [
-                [3, 5],
-                [4, 4],
-                [5, 5]
+                [3, 5], [4, 4], [5, 5]
+            ],
+            'water': [
+                [3, 3],[4, 2], [5, 3]
             ]
         },
         'territories': {
             'cougar': [
-                [5, 3],
-                [5, 5]
+                [5, 3], [5, 5]
+            ]
+        }
+    },
+    # Piece 5
+    {
+        'terrain': {
+            'desert': [
+                [0, 4], [1, 3], [1, 5], [2, 2]  
+            ],
+            'mountain': [
+                [3, 1], [4, 0], [4, 2], [5, 1], [5, 3]
+            ],
+            'swamp': [
+                [0, 0], [0, 2], [1, 1], [2, 0] 
+            ],
+            'water': [
+                [2, 4], [3, 3], [3, 5], [4, 4], [5, 5]
             ]
         },
-        'structures': {
-            '5-3': ['shack', 'blue'],
-            '5-5': ['shack', 'blue'],
-            '4-0': ['stone', 'red']
+        'territories': {
+            'bear': [
+                [4, 4], [5,3], [5, 5]
+            ]
         }
-    }
+    },
+    # Piece 6
+    {
+        'terrain': {
+            'desert': [
+                [0, 0], [1, 1]
+            ],
+            'forest': [
+                [4, 2], [5, 1], [5, 3], [5, 5]
+            ],
+            'mountain': [
+                [0, 2], [0, 4], [1, 3]
+            ],
+            'swamp': [
+                [2, 0], [2, 2], [3, 1], [3, 3], [4, 0]
+            ],
+            'water': [
+                [1, 5], [2, 4], [3, 5], [4, 4]
+            ]
+        },
+        'territories': {
+            'bear': [
+                [0, 0], [0, 2]
+            ]
+        }
+    },
 ]
 
+structures = {
+    '1-3': ['stone', 'white'],
+    '7-1': ['stone', 'blue'],
+    '10-4': ['shack', 'green'],
+    '5-9': ['stone', 'green'],
+    '4-12': ['shack', 'white'],
+    '11-13': ['shack', 'blue']
+}
+
+def validateConfig(pieces):
+    requiredCells = [x for x in product(range(0, 5, 2), repeat=2)] + [x for x in product(range(1, 6, 2), repeat=2)]  
+    for (i, piece) in enumerate(pieces):
+        if i != 0:
+            allCells = []
+            for environment in piece['terrain']:
+                allCells += piece['terrain'][environment]
+            for cell in requiredCells:
+                if list(cell) not in allCells:
+                    print(f'Environment {i} missing cell {cell}')
 if __name__ == '__main__':
+    validateConfig(pieces)
     test = Board(**{
-        'dims': {'cols':6, 'rows':6, 'length':80},
-        'terrain': pieces[4]['terrain'],
-        'territories': pieces[4]['territories'],
-        'structures': pieces[4]['structures']
+        'dims': {'cols':12, 'rows':18, 'length':40},
+        'pieces': pieces,
+        'pieceOrder': [4, -5, 3, -2, 6, 1],
+        'structures': structures,
+        'origin': {'x': 100, 'y': 50}
     })
     test.drawBoard()
